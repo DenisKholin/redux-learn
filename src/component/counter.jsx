fc9607988012d0caa020904b6995474db4fda26b
Denis Kholin <den07.06@yandex.ru>
2022-12-24 19:35:38 +0300

    lesson 1

---
+++ src/component/counter.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Counter = ({ counter, dec, inc, rnd }) => {
+	return (
+		<div className="jumbotron">
+			<h1 id="counter">{counter}</h1>
+			<button onClick={dec} className="btn btn-primary">DEC</button>
+			<button onClick={inc} className="btn btn-primary">INC</button>
+			<button onClick={rnd} className="btn btn-primary">RND</button>
+		</div>
+	)
+}
+
+const mapStateToProps = (state) => {
+	return {
+		counter: state
+	}
+}
+
+export default connect(mapStateToProps, actions)(Counter);
