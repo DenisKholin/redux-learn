5fcd70182fe0f5de7e2890ba8e7994eb160fefa3
Denis Kholin <den07.06@yandex.ru>
2022-12-26 18:00:52 +0300

    reject the concept of 'index'

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react';
-import MenuListItem from '../menu-list-item';
+import React, { Component } from 'react';
+import MenuListItem from '../menu-list-item/menu-list-item';
 
 import './menu-list.scss';
 
 class MenuList extends Component {
 
-    render() {
+	render() {
 
-        return (
-            <ul className="menu__list">
-                <MenuListItem/>
-            </ul>
-        )
-    }
+		return (
+			<ul className="menu__list">
+				<MenuListItem />
+			</ul>
+		)
+	}
 };
 
 
